539b0d5d35f4be981c2a09ba76407e8de3494582
kramstyles <mark.eke@decagon.dev>
2022-05-12 20:18:57 +0100

    addded api app

--- project/settings.py
+++ project/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # APPS
+    'api.apps.ApiConfig',
+
     # PIP INSTALLS
     'rest_framework',
     'drf_yasg',
